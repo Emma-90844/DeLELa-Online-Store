1d121a5107caa79524091a949d9975c265e89966
Emma-90844 <enabojohn@gmail.com>
2022-09-22 19:53:07 +0300

    Added firbase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4-DtfCoq1RJ107mqFM88FobIIloyghC0",
+    authDomain: "dilela-8ad60.firebaseapp.com",
+    projectId: "dilela-8ad60",
+    storageBucket: "dilela-8ad60.appspot.com",
+    messagingSenderId: "989848500132",
+    appId: "1:989848500132:web:0900ca388e05d32376f331"
+  };
