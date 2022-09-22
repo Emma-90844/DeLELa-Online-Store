0e2b4cd51d15fdf6e540d60e509fe87aefad13d1
Emma-90844 <enabojohn@gmail.com>
2022-09-22 16:04:05 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Header from './../components/Header';
+import Banner from './../components/Banner'
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({products}) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>DiLEla</title>
+      </Head>
+      {/* header component */}
+      <Header />
+      {/* max-w-screen-2xl mx-auto  */}
+   
+      <main className="max-w-6xl mx-auto">
+  
+        <Banner/>
+        <ProductFeed products={products}/>
+      </main>
+   
+    </div>
+  );
+}
+
+// Next JS Server side Rendering
+export async function getServerSideProps(context){
+  const products =  await fetch("https://fakestoreapi.com/products").then((res) => res.json());
+  return {props: {
+    products: products
+  }}
+}
+
