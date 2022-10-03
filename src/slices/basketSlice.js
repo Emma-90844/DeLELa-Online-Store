e9a9f7eace7a917389be14403f1a1eae8dcae8ba
Emma-90844 <enabojohn@gmail.com>
2022-10-03 13:29:44 +0300

    Added reduce to iterate through the item price to give total basket amount

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -9,9 +9,23 @@ export const basketSlice = createSlice({
   initialState,
   reducers: {
     addToBasket: (state, action) => {
-      state.items = [...state.items, action.payload]
+      state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+      let newBasket  = [...state.items]
+      if(index >= 0 ){
+        newBasket.splice(index, 1)
+        // the item exixt remove else the itel doesnt exist
+      }else{
+        console.warn(
+          `cant delete the product`
+        )
+      }
+      state.items = newBasket
+    },
   },
 });
 
@@ -19,5 +33,8 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
+
+// total . iterate through the array
+export const selectTotal = (state) => state.basket.items.reduce((total, item) => total + item.price, 0)
 
 export default basketSlice.reducer;
