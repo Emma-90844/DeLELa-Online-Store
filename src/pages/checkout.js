4e8bd8dd5c29f93c1c3296a7e9cf83873ce83836
Emma-90844 <enabojohn@gmail.com>
2022-10-03 13:30:31 +0300

    Added Add/Remove button and redux functionality

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import React from "react";
 import Header from "../components/Header";
+import { useSelector } from "react-redux";
+import { selectItems, selectTotal } from "./../slices/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
-function checkout() {
+function checkout({}) {
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal)
+  const { data: session } = useSession();
   return (
     <div className="bg-gray-100">
       <Header />
@@ -17,13 +25,52 @@ function checkout() {
           />
           <div className="flex flex-col p-5 mb-3 space-y-10 bg-white">
             <h1 className="border-b pb-4 text-2xl ">
-              Your Shopping Basket
+              {items.length === 0
+                ? "Sorry your Basket is Empty"
+                : "Shoping Basket"}
             </h1>
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
           </div>
         </div>
 
         {/* right hand */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="">
+                SUBTOTAL{" "}
+                <p className="">
+                  ({items.length} items) 
+                  <br/>
+                  <span className="font-bold">
+                    <Currency quantity={total} currency="UGX"/>
+                  </span>
+                </p>
+              </h2>
+              <button
+              disabled={!session}
+                className={`button mt-3 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-yellow-400 text-gray-300 cursor-not-allowed "
+                } `}
+              >
+                {!session ? "Signin To Checkout" : "Proceed to Checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
